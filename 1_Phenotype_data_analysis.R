d591e654174774f86feab2988f8a85f77d4b600d
Evelia Lorena Coss-Navarrete <49405921+EveliaCoss@users.noreply.github.com>
2024-09-03 11:54:07 -0600

    Update

--- 1_Phenotype_data_analysis.R
+++ 1_Phenotype_data_analysis.R
@@ -1,14 +1,21 @@
 # Analysis of bacterial genomes evolved in the absence of phage
 # Reena Debray
 # Feb 1, 2022
+# Modified: Evelia Coss (03/Sep/2024)
 
 ## This function uses the fit_growthmodel() function of the growthrates package to calculate growth rates from microplate data
 ## It takes data in the following form: each row is the measurement of a single well at a single time point, with additional columns "Treatment" and "OD".
 ## A logistic curve is fit to each well separately using the OD over time
 ## I used the following parameters: lower=c(y0=0.000001,mumax=0,K=0),upper=c(y0=0.05,mumax=5,K=1.5)),p=c(y0=0.01,mumax=0.2,K=0.1)
 
+# --- packages ----
 library(growthrates)
 library(lmerTest)
+library(readxl)
+library(tidyverse)
+library(reshape2)
+
+#---- GC_long_to_growthrates() -------
 
 GC_long_to_growthrates<-function(GC_long,lower,upper,p){
   ###initialize data frame
@@ -25,9 +32,10 @@ GC_long_to_growthrates<-function(GC_long,lower,upper,p){
   return(growthrates)
 }
 
-## Figure 1: Resistance is costly
+## ----- Figure 1: Resistance is costly -------
 
 ### Read in "Costs_of_Res.xlsx" as costs_of_res
+costs_of_res <- read_excel("Costs_of_Res.xlsx")
 ### Calculate fitted values controlling for phage resistance and plate layout
 ### Express fitted values as a percentage of wild-type fitness
 costs_of_res$fitted<-fitted.values(lm(r~(Population=="ancDC3000")+Column+Plate,costs_of_res))
@@ -50,8 +58,9 @@ anova(lm(fitted_percWT~Gene,costs_agg[costs_agg$Population!="MS15",]))
 ggplot(costs_of_res[costs_of_res$Population!="ancDC3000",])+stat_summary(aes(reorder(Population,-fitted_percWT),fitted_percWT,group=Population,fill=Gene),geom="pointrange",shape=21,size=0.8)+theme_classic(base_size=18)+theme(axis.title.x=element_blank(),axis.text.x=element_blank(),axis.ticks.x=element_blank())+guides(fill=F)+facet_grid(~paste(Gene,Annotation,sep="\n"),scales="free_x",space="free_x")+theme(strip.background = element_blank(),strip.placement="outside")+scale_fill_brewer(palette="Dark2")+ylab("Fitness (% of wild-type)")+geom_hline(yintercept=100,linetype="dashed")
 
 
-## Figure 2A: Growth over time
+## ------- Figure 2A: Growth over time ---------
 ### Read in "Fitness_over_Time.xlsx" as fitness_over_time
+fitness_over_time <- read_excel("Fitness_over_Time.xlsx")
 ### Calculate fitted values controlling for phage resistance, passage, and plate layout
 fitness_over_time$fitted<-fitted.values(lm(r~Passage*Type+Column,fitness_over_time))
 ### Express fitted values as a percentage of wild-type fitness
@@ -66,7 +75,7 @@ colnames(df)=c("passage","mean","SD")
 ggplot()+geom_point(data=df,aes(x=passage*3,y=mean),size=3)+geom_errorbar(data=df,aes(x=passage*3,ymin=mean-SD,ymax=mean+SD),width=0,size=1)+geom_line(data=df,aes(x=passage*3,y=mean))+theme_classic(base_size=16)+geom_hline(yintercept=100,linetype="dashed")+scale_x_continuous(breaks=seq(0,36,6))+ylab("Fitness (% of wild-type)")+xlab("Day of experimental evolution")+coord_cartesian(ylim=c(60,115))
 
 
-## Figure 2B: Resistance over time
+## ------ Figure 2B: Resistance over time -------
 ### Read in "Resistance_over_Time.xlsx" as res_over_time
 res_long<-melt(res_over_time,id.vars = "isolate",measure.vars = c("Prop_S_P0","Prop_S_P4","Prop_S_P8","Prop_S_P12"))
 res_long$variable<-as.character(res_long$variable)
@@ -83,15 +92,22 @@ costs_agg[!costs_agg$Population%in%c("SNK7","QAC5","VCM4"),"Outcome"]<-"R"
 t.test(fitted_percWT~Outcome,costs_agg)
 
 
-### Code for Figure 2B
+### ----- Code for Figure 2B -----
+# It could have been an additional transformation of res_over_time or a similar dataset.
+LOR_res_long <- melt(res_over_time, id.vars = "isolate", measure.vars = c("Prop_S_P0","Prop_S_P4","Prop_S_P8","Prop_S_P12"))
+LOR_res_long$variable <- as.character(LOR_res_long$variable)
+LOR_res_long$passage <- as.numeric(substr(LOR_res_long$variable,9,nchar(LOR_res_long$variable)))
+LOR_res_long <- LOR_res_long[!LOR_res_long$isolate %in% c("FMS6","SNK6"),]  # Insulation filtering
+
 ### Excludes populations with colonies that appeared sensitive on plates but were not affected by phage in growth curves
 ggplot(LOR_res_long[!LOR_res_long$isolate%in%c("FMS6","SNK6"),],aes(passage*3,value*100,group=isolate,color=(isolate%in%c("SNK7","QAC5","VCM4"))))+geom_point(size=4.5,shape=18)+geom_line(size=0.8)+theme_classic()+guides(color=F)+scale_color_brewer(palette="Dark2")+scale_x_continuous(breaks=c(0,12,24,36))+theme_classic(base_size=16)+ylab("Proportion of phage sensitivity in population (%)")+xlab("Day of experimental evolution")+theme(axis.title.y=element_text(size=14))
 
-## Figure 3: Replay experiment
+## ----- Figure 3: Replay experiment -------
 ### Read in "Replay.xlsx" as replay
-
+replay <- read_excel("Replay.xlsx", sheet = "Data")
+replay$Prop_S_P12 <- as.numeric(as.character(replay$Prop_S_P12)) # Convert as numeric
 ### Populations derived from a founder that re-evolved sensitivity were more likely to re-evolve sensitivity as well
-t.test(Prop_R_P12~Founder_outcome,data=replay)
+t.test(Prop_S_P12~Founder_outcome,data=replay)
 
 ### Code for Figure 3C
 agg<-data.frame(aggregate(100*replay$Prop_S_P12,by=list(replay$Founder_outcome),FUN=function(x){mean(x,na.rm=T)}),aggregate(100*replay$Prop_S_P12,by=list(replay$Founder_outcome),FUN=function(x){sd(x,na.rm=T)/sqrt(length(x))}))
