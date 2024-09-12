b48e226195c64096fa30aa19f8c626f73cce3914
Amairani Cancino Bello <83901707+acancinob@users.noreply.github.com>
2024-09-11 19:19:31 -0600

    Update 2_Genomes_data_analysis.R

--- 2_Genomes_data_analysis.R
+++ 2_Genomes_data_analysis.R
@@ -18,9 +18,9 @@
 #' @import ggplot2: This is one of the most popular packages in R for data visualization. You can create basic plots (such as histograms and bar charts) as well as more complex visualizations (like heatmaps and violin plots).
 #' @import viridis : This package provides color palettes for plots in R. The viridis palettes are particularly useful in visualizations like heatmaps or scatter plots where color differentiation is important.
 
+install.packages("viridis")
 library(readxl)
 library(ggplot2)
-install.packages("viridis")
 library(viridis)
 
 
